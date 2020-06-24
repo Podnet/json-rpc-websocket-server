44f93113be47067cf858543829ebf3ceee4dbaf8
Apoorva Singh <apoorvasingh157@gmail.com>
2020-06-25 00:46:13 +0530

    Requesting to execute 'ping' function

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import asyncio
+
 import websockets
+from loguru import logger
 from jsonrpcclient.clients.websockets_client import WebSocketsClient
 
 
 async def main():
     async with websockets.connect("ws://localhost:5000") as ws:
         response = await WebSocketsClient(ws).request("ping")
-        print(response)
+        # await ws.send("ping")
+        # response = await ws.recv()
+        logger.info(f"Response: {response.data}")
 
 
 asyncio.get_event_loop().run_until_complete(main())
