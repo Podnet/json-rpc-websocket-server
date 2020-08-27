b6b726e10cab62910718fb674f2f69b05df6b8b7
Apoorva Singh <apoorvasingh157@gmail.com>
2020-08-27 16:15:23 +0530

    tcu.py can send and recv msgs independently

--- tcu.py
+++ tcu.py
@@ -6,13 +6,25 @@ from jsonrpcclient.requests import Request
 
 SERVER_URL = "ws://localhost:7000"
 
-async def hello():
+async def send_sensor_data(websocket):
+    req = Request("sensor_data", values="[1,2,3,4,5,6,7,8,9,10]")
+    await websocket.send(str(req))
+    print("Msg sent")
+    await asyncio.sleep(1)
+
+async def recv_msg(websocket):
+    while True:
+        print(await websocket.recv())
+
+
+async def handler():
     
     async with websockets.connect(SERVER_URL) as websocket:
-        while True:
-            req = Request("sensor_data", values="[1,2,3,4,5,6,7,8,9,10]")
-            await websocket.send(str(req))
-            print(await websocket.recv())
-            await asyncio.sleep(1)
+        task1 = asyncio.create_task(send_sensor_data(websocket))
+        task2 = asyncio.create_task(recv_msg(websocket))
 
-asyncio.get_event_loop().run_until_complete(hello())
+        await task1
+        await task2
+
+
+asyncio.get_event_loop().run_until_complete(handler())
